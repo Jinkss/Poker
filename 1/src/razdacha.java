f44a1c2585629b9fa0a75b7999592943104e5ada
Jinkss <peregorodovakaterina@yandex.ru>
2015-03-20 01:21:20 +0300

    2

--- 1/src/razdacha.java
+++ 1/src/razdacha.java
@@ -1,9 +1,30 @@
 
 public class razdacha {
 
+ int count;
+  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		if (count<5) {								
+			//цикл раздачи
+		Player1;
+		Player2;
+		Player3;
+		Player4;
+		Player5;
+			count=count+1;
+		}
+		
+		
+		public  GeneriruemKarti() {
+			razdacha []	 Nominal = new razdacha [5]; //массив числовых карт
+			
+			String [] Masti = {"Черви","Вини","Крести","Буби"}; //массив по мастям
+			String [] Kartinki = {"Валет","Дама","Король","Туз"}; 
+			
+			int random_karta = (int (Math.random()*4));
+		}
 	}
 
 }
